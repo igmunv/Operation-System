6dfeb8c04cabf25a1065d120d0f9de01270d3cb1
igmunv <devskynger@gmail.com>
2025-09-27 04:15:57 -0400

    add stoi, strlen, is_digit, smtoi

--- source/string.c
+++ source/string.c
@@ -62,3 +62,65 @@ void itos(long i, unsigned char* ret){
     }
     return;
 }
+
+char is_digit(char* string){
+    for (char i = 0; i < 100; i++){
+        char symbol = string[i];
+        if (symbol == '\0') break;
+        switch (symbol){
+            case '0': continue;
+            case '1': continue;
+            case '2': continue;
+            case '3': continue;
+            case '4': continue;
+            case '5': continue;
+            case '6': continue;
+            case '7': continue;
+            case '8': continue;
+            case '9': continue;
+            default: return 0;
+        }
+    }
+    return 1;
+}
+
+short strlen(char* string){
+    short len = 0;
+     for (char i = 0; i < 100; i++){
+        char symbol = string[i];
+        if (symbol == '\0') break;
+        len++;
+    }
+    return len;
+}
+
+char smtoi(char symbol){
+    switch (symbol){
+        case '0': return 0;
+        case '1': return 1;
+        case '2': return 2;
+        case '3': return 3;
+        case '4': return 4;
+        case '5': return 5;
+        case '6': return 6;
+        case '7': return 7;
+        case '8': return 8;
+        case '9': return 9;
+        default: return 0;
+    }
+}
+
+char stoi(char* string, int* result){
+    if (!is_digit(string)){
+        return 0;
+    }
+    int i_result = 0;
+    int umn = 1;
+    short len = strlen(string);
+    for (char i = len-1; i >= 0; i--){
+        char symbol = string[i];
+        char digit = smtoi(symbol);
+        i_result = (digit * umn) + i_result;
+        umn *= 10;
+    }
+}
