17089997ca842e4e32b7d259a20d0d63eb1d47e3
igmunv <devskynger@gmail.com>
2025-10-04 12:54:30 +0500

    modified sleep function in time.c

--- src/lib/time.c
+++ src/lib/time.c
@@ -1,7 +1,9 @@
+// Special memory: ticks
+volatile unsigned long* ptticks = (unsigned long*)0x5018;
 
 
 // Уснуть, ждать
 void sleep(int ms){
-    unsigned long long ticks_local = ticks;
-    while ((ticks-ticks_local) <= ms){asm("hlt");}
+    unsigned long long ticks_local = *ptticks;
+    while ((*ptticks-ticks_local) <= ms){asm("hlt");}
 }
